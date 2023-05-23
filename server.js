1d0a674555dbe2a56e3e7a8af107f7571ce6b1d1
jairobmiranda <jairo17.mb@gmail.com>
2023-05-23 14:32:04 -0300

    inclusao do server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = 80;
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
