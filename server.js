6c076c6df45fd8188f3d90c678ade34ddcc25c29
jairobmiranda <jairo17.mb@gmail.com>
2023-05-23 14:40:08 -0300

    correção

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'projeto-facil-front/public')));
 
 const port = 80;
 app.listen(port, () => {
